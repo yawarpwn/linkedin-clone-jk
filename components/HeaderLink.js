7bb64f532318881ac44641f796c7185f26dbadbe
yawarpwn <john.kazz@gmail.com>
2022-06-15 18:25:37 -0500

    added darkmode toggle with tailwindcss

--- components/HeaderLink.js
+++ components/HeaderLink.js
@@ -1,11 +1,38 @@
-export default function HeaderLink({ Icon, text, avatar, feed }) {
+import { useSession, signOut } from 'next-auth/react'
+
+function HeaderLink({ Icon, text, feed, active, avatar, hidden }) {
+  // const { data: session } = useSession()
+
   return (
-    <div className={`flex flex-col cursor-pointer justify-center items-center
-${feed ? 'text-black/60 hover:text-black space-y-1 lg:-mb-1.5' 
-: 'text-gray-500 hover:text-gray-700'}
-`}>
-      {avatar ? <Icon className="!h-7 !w-7 lg:!mb-7" /> : <Icon />}
-      <h4 className="text-sm">{text}</h4>
+    <div
+      className={`${
+        hidden && 'hidden md:inline-flex'
+      } flex cursor-pointer flex-col items-center justify-center ${
+        feed
+          ? 'space-y-1 text-black/60 hover:text-black dark:text-white/75 dark:hover:text-white lg:-mb-1.5'
+          : 'text-gray-500 hover:text-gray-700'
+      } ${active && '!text-black dark:!text-white'}`}
+      onClick={() => avatar && signOut()}
+    >
+      {avatar ? (
+        <Icon className="!h-7 !w-7 lg:!-mb-1"  />
+      ) : (
+        <Icon />
+      )}
+
+      <h4
+        className={`text-sm ${
+          feed && 'mx-auto hidden w-full justify-center lg:flex'
+        }`}
+      >
+        {text}
+      </h4>
+
+      {active && (
+        <span className="hidden h-0.5 w-[calc(100%+20px)] rounded-t-full bg-black dark:bg-white lg:inline-flex" />
+      )}
     </div>
   )
 }
+
+export default HeaderLink
