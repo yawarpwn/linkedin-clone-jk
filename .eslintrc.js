f89eea2cac4aca12d22020f21a864eb42184e983
yawarpwn <john.kazz@gmail.com>
2022-07-03 12:50:49 -0500

    vital eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,29 @@
+// module.exports = {
+//     "env": {
+//         "browser": true,
+//         "es2021": true,
+//         "node": true
+//     },
+//     "extends": [
+//         "eslint:recommended",
+//         "plugin:react/recommended"
+//     ],
+//     "parserOptions": {
+//         "ecmaFeatures": {
+//             "jsx": true
+//         },
+//         "ecmaVersion": "latest",
+//         "sourceType": "module"
+//     },
+//     "plugins": [
+//         "react"
+//     ],
+//     "rules": {
+//     "react/react-in-jsx-scope": "off",
+//     "react/prop-types": "off"
+//     }
+// }
+
 module.exports = {
-    // "env": {
-    //     "browser": true,
-    //     "es2021": true,
-    //     "node": true
-    // },
-    // "extends": [
-    //     "eslint:recommended",
-    //     "plugin:react/recommended"
-    // ],
-    // "parserOptions": {
-    //     "ecmaFeatures": {
-    //         "jsx": true
-    //     },
-    //     "ecmaVersion": "latest",
-    //     "sourceType": "module"
-    // },
-    // "plugins": [
-    //     "react"
-    // ],
-    // "rules": {
-    // "react/react-in-jsx-scope": "off",
-    // "react/prop-types": "off"
-    // }
+  extends: "next/core-web-vitals"
 }
